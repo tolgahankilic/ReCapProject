5bd0fa3d5a3d58d677179dd1096dd2d22692db64
Tolgahan Kılıç <kilictolgahan@outlook.comgit configgit config --global user.email kilictolgahan@outlook.com>
2021-02-24 01:40:25 +0300

    ValidationAspect added

--- Business/Concrete/RentalManager.cs
+++ Business/Concrete/RentalManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
@@ -20,9 +21,9 @@ namespace Business.Concrete
             _rentalDal = rentalDal;
         }
 
+        [ValidationAspect(typeof(RentalValidator))]
         public IResult Add(Rental rental)
         {
-            ValidationTool.Validate(new RentalValidator(), rental);
             if (_rentalDal.GetRentalDetails(r => r.CarId == rental.CarId).Count > 0)
             {
                 return new ErrorResult(Messages.CarAlreadyRented);
