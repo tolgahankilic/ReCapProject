02552fe9fe6367242d851ec69259d2cac28afa3b
Tolgahan Kılıç <kilictolgahan@outlook.comgit configgit config --global user.email kilictolgahan@outlook.com>
2021-03-24 23:28:20 +0300

    Cors added

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -88,6 +88,8 @@ namespace WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
